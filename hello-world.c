0928a2436c599aa3bef8894b9c381eb3b62714ed
sakthiprabhu-k <sprabhu8870k@gmail.com>
2023-09-02 17:22:33 +0530

    Updated gitignore and hello-world.c files

--- hello-world.c
+++ hello-world.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 
+
 int main() {
-    printf("Hello World!\n");
+    printf("----------------\n");
+    printf("Hello World!!\n");
+    printf("----------------\n");
     return 0;
 }
